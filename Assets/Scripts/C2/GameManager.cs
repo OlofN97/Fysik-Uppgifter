fbf411fd1d83a896e928270dc7e9e571340e1d08
OlofN <Olof_norrlind@hotmail.com>
2022-04-26 12:42:12 +0200

    C2

--- Assets/Scripts/C2/GameManager.cs
+++ Assets/Scripts/C2/GameManager.cs
@@ -1,31 +1,43 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     [SerializeField] private GameObject car;
     [SerializeField] private float staticFriciton;
+    [SerializeField] private float speed;
+    [SerializeField] private Camera cam;
+
     private float radius = 30;
     private bool running;
     private bool onRoad;
-    [SerializeField] private float speed;
     private float maxSpeed;
     private const float gravitation = 9.82f;
-
 
     private float circumference;
     private float distanceTraveled;
     private float angleTraveled;
 
+    [Header("UI")]
+    [SerializeField] private Text speedText;
+    [SerializeField] private Slider speedInput;
+    [SerializeField] private InputField radieInput;
+    [SerializeField] private Text frictionText;
+  
 
     void Start()
     {
         running = false;
+        frictionText.text = "Friction: " + staticFriciton;
+        speedText.text = "Speed: " + speedInput.value;
     }
 
     void Update()
     {
+
+        speed = speedInput.value;
+
         if (running)
         {
             UpdateMovement();
@@ -39,22 +51,22 @@ public class GameManager : MonoBehaviour
 
         if (onRoad)
         {
-
-
-
             distanceTraveled += speed * Time.deltaTime;
             angleTraveled = (distanceTraveled / circumference) * 360 * Mathf.Deg2Rad;
             car.transform.position = new Vector3(Mathf.Cos(angleTraveled) * radius, Mathf.Sin(angleTraveled) * radius, 0);
         }
-        if (onRoad)
+        if (!onRoad)
         {
-            //Kod för hur den ska köra här
+            car.transform.position += car.transform.up *speed *Time.deltaTime;
         }
     }
     private void UpdateRotation()
     {
-        Vector3 rotation = new Vector3(0, 0, (speed * Time.deltaTime / circumference) * 360);
-        car.transform.Rotate(rotation);
+        if (onRoad)
+        {
+            Vector3 rotation = new Vector3(0, 0, (speed * Time.deltaTime / circumference) * 360);
+            car.transform.Rotate(rotation);
+        }
     }
 
     private void SetMaxSpeed()
@@ -69,10 +81,32 @@ public class GameManager : MonoBehaviour
 
     public void StartButton()
     {
+        GetInputs();
+        SetMaxSpeed();
+
         car = Instantiate(car, transform.position + new Vector3(radius, 0, 0), Quaternion.identity);
         running = true;
         onRoad = true;
         circumference = Mathf.PI * 2 * radius;
-        SetMaxSpeed();
+
+        cam.orthographicSize = radius;
+
     }
+
+    private void GetInputs()
+    {
+        radius = float.Parse(radieInput.text);
+        speed = speedInput.value;
+    }
+
+    public void ResetScene()
+    {
+        SceneManager.LoadScene("C2");
+    }
+
+    public void OnSpeedChange()
+    {
+        speedText.text = "Speed: " + speedInput.value;
+    }
+    
 }
